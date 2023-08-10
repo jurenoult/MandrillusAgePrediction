05743b039c0827cd1ae1f430f3c0f2b031960942
Romain Karpinski <romain.karpinski@loria.fr>
2023-08-10 10:12:46 +0200

    feat: updated vgg face model to be able to load pretrained weights

--- mandrillage/models.py
+++ mandrillage/models.py
@@ -507,55 +507,75 @@ class InceptionResnetV1(nn.Module):
 class VGGFace(nn.Module):
     def __init__(self, start_filters=64, output_dim=2622):
         super().__init__()
-        self.model = nn.Sequential(
-            self.conv_block(3, start_filters, kernel_size=3, zero_pad=1),
-            self.conv_block(start_filters, start_filters, kernel_size=3, zero_pad=1),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            self.conv_block(
-                start_filters, start_filters * 2, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 2, start_filters * 2, kernel_size=3, zero_pad=1
-            ),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            self.conv_block(
-                start_filters * 2, start_filters * 4, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 4, start_filters * 4, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 4, start_filters * 4, kernel_size=3, zero_pad=1
-            ),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            self.conv_block(
-                start_filters * 4, start_filters * 8, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
-            ),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            self.conv_block(
-                start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
-            ),
-            self.conv_block(
-                start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
-            ),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            self.conv_block(start_filters * 8, start_filters * 64, kernel_size=7),
-            nn.Dropout(0.5),
-            self.conv_block(start_filters * 64, start_filters * 64, kernel_size=1),
-            nn.Dropout(0.5),
-            nn.Conv2d(start_filters * 64, output_dim, kernel_size=1),
-        )
 
-        self.output_dim = output_dim
+        self.block_size = [2, 2, 3, 3, 3]
+        self.conv_1_1 = nn.Conv2d(3, 64, 3, stride=1, padding=1)
+        self.conv_1_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.conv_2_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)
+        self.conv_2_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)
+        self.conv_3_1 = nn.Conv2d(128, 256, 3, stride=1, padding=1)
+        self.conv_3_2 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv_3_3 = nn.Conv2d(256, 256, 3, stride=1, padding=1)
+        self.conv_4_1 = nn.Conv2d(256, 512, 3, stride=1, padding=1)
+        self.conv_4_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_4_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_1 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_2 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.conv_5_3 = nn.Conv2d(512, 512, 3, stride=1, padding=1)
+        self.fc6 = nn.Linear(512 * 7 * 7, 4096)
+        self.fc7 = nn.Linear(4096, 4096)
+        self.fc8 = nn.Linear(4096, 2622)
+
+        # self.model = nn.Sequential(
+        #     self.conv_block(3, start_filters, kernel_size=3, zero_pad=1),
+        #     self.conv_block(start_filters, start_filters, kernel_size=3, zero_pad=1),
+        #     nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        #     self.conv_block(
+        #         start_filters, start_filters * 2, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 2, start_filters * 2, kernel_size=3, zero_pad=1
+        #     ),
+        #     nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        #     self.conv_block(
+        #         start_filters * 2, start_filters * 4, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 4, start_filters * 4, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 4, start_filters * 4, kernel_size=3, zero_pad=1
+        #     ),
+        #     nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        #     self.conv_block(
+        #         start_filters * 4, start_filters * 8, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
+        #     ),
+        #     nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        #     self.conv_block(
+        #         start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
+        #     ),
+        #     self.conv_block(
+        #         start_filters * 8, start_filters * 8, kernel_size=3, zero_pad=1
+        #     ),
+        #     nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        #     self.conv_block(start_filters * 8, start_filters * 64, kernel_size=7),
+        #     nn.Dropout(0.5),
+        #     self.conv_block(start_filters * 64, start_filters * 64, kernel_size=1),
+        #     nn.Dropout(0.5),
+        #     nn.Conv2d(start_filters * 64, output_dim, kernel_size=1),
+        # )
+
+        # self.output_dim = output_dim
+        self.output_dim = 2622
 
     def conv_block(self, input_dim, output_dim, kernel_size, zero_pad=0):
         conv = nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size)
@@ -602,7 +622,33 @@ class VGGFace(nn.Module):
         print(f"Loaded model from pretrained model at path : {path}")
 
     def features(self, x):
-        x = self.model(x)
+        x = F.relu(self.conv_1_1(x))
+        x = F.relu(self.conv_1_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_2_1(x))
+        x = F.relu(self.conv_2_2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_3_1(x))
+        x = F.relu(self.conv_3_2(x))
+        x = F.relu(self.conv_3_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_4_1(x))
+        x = F.relu(self.conv_4_2(x))
+        x = F.relu(self.conv_4_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv_5_1(x))
+        x = F.relu(self.conv_5_2(x))
+        x = F.relu(self.conv_5_3(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc6(x))
+        x = F.dropout(x, 0.5, self.training)
+        x = F.relu(self.fc7(x))
+        x = F.dropout(x, 0.5, self.training)
+        x = self.fc8(x)
+
+        # x = self.model(x)
+
         x = torch.flatten(x, start_dim=1)
         return x
 
