fix: wrong loss weighting

mandrillage/losses.py
@@ -53,6 +53,7 @@ class LinearWeighting(nn.Module):
         # error_ratio = a * 1 + b
         # error_ratio - b = a
         self._a = self.error_ratio - 1
+        # self._a = 1 - self.error_ratio
 
         # self._a = torch.nn.Parameter(torch.tensor(self.error_ratio))
         # self.activation = nn.Sigmoid()
@@ -65,7 +66,8 @@ class LinearWeighting(nn.Module):
         # However if we add the weight to the loss, a=1 is a penality
 
     def get_weight(self, x):
-        weight = -self.a * x + self.b
+        weight = self.a * x + self.b
+        # weight = 1 - self.a * x**2
         return weight
 
     def forward(self, y_pred, y_true):