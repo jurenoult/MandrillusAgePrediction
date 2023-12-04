fe2ebb136ff6ab9def199f8dbedfb74e44a02a48
Romain Karpinski <romain.karpinski@loria.fr>
2023-12-04 16:43:46 +0100

    refactor: added parameters to inference script

--- inference.py
+++ inference.py
@@ -1,10 +1,12 @@
 import numpy as np
 import torch
-from mandrillage.models import DinoV2, SequentialModel, RegressionHead
+from mandrillage.models import DinoV2
+import click
+from skimage import io
 
 
 def preload_backbone():
-    DinoV2("medium")
+    DinoV2("small")
 
 
 def load_model(backbone_name, weights_path):
@@ -33,22 +35,23 @@ def inference(model, image, device):
     return prediction.detach().cpu().numpy()
 
 
-def test():
-    model = load_model(
-        "dinov2_medium",
-        "/home/rkarpins/projects/mandrill/mandrillagerecognition/logs/experiments/runs/regression_freeze/checkpoints/regression_0_regression_freeze.h5",
-    )
-    from skimage import io
-
-    image = io.imread(
-        "/home/rkarpins/projects/mandrill/mandrillagerecognition/data/db2/Images/MANDRILLS CIRMF/17B7B/20180802_id17B7B_malsub_(1)_ORIGINAL.jpg"
-    )
-
-    print(model)
-
+@click.command()
+@click.option(
+    "--model_path",
+    required=True,
+    help="Path to model to load.",
+)
+@click.option(
+    "--image_path",
+    required=True,
+    help="Path to image to use.",
+)
+def main(model_path, image_path):
+    model = load_model("dinov2_medium", model_path)
+    image = io.imread(image_path)
     age = inference(model, image, torch.device("cuda"))
     print(f"Predicted age: {age}")
 
 
 if __name__ == "__main__":
-    test()
+    main()
